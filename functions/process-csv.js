ecfa70d9b3745e565e26985eb3b580c2840f7cea
ncado <vitaliy.mistriakov@uatechpeople.com>
2023-05-25 03:38:12 +0300

    sms work

--- functions/process-csv.js
+++ functions/process-csv.js
@@ -76,24 +76,73 @@ const validateArray = (arr) => {
 }
 
 
+// const sendItemsToSqs = async (items) => {
+//
+//
+//     for (let i = 0; i < items.length;) {
+//         let params = {
+//             QueueUrl: process.env.SQS_QUEUE_URL,
+//             Entries: [],
+//         };
+//
+//         for (let j = 0; j < 10 && i < items.length; j++, i++) {
+//             params.Entries.push({
+//                 Id: i.toString(),
+//                 MessageBody: JSON.stringify({
+//                     userId: items[i].item.UserAccountId,
+//                     field: 'OptedInThirtyDaysCancellation',
+//                     value: items[i].item.Preferences.OptedInThirtyDaysCancellation,
+//                 }),
+//             });
+//         }
+//
+//         console.log("sqs.sendMessageBatch(params).+++++", params.Entries);
+//
+//         await sqs.sendMessageBatch(params).promise();
+//     }
+//
+//
+// };
+
+
 const sendItemsToSqs = async (items) => {
+    let successCount = 0;
+    const messageBatchSize = 50
+    const awsBatchSize = 10
+
+    items = items.map((item) => {
+        return {
+
+            userId: item.item.UserAccountId,
+            field: 'OptedInThirtyDaysCancellation',
+            // LastUpdated: item.item.LastUpdated,
+            //Preferences: item.item.Preferences,
+            value: false
+            // MarketingFlags: item.item.MarketingFlags
+        }
+    })
+
+    const messageBatches = chunk(items, messageBatchSize); // масиви чанків масивів
 
 
-    for (let i = 0; i < items.length;) {
-        let params = {
+    console.log("messageBatches[]----->", messageBatches[0])
+    console.log("messageBatches----->", messageBatches)
+
+    const sqsMessagePayloads = messageBatches.map((batch, index) => {
+        console.log("batch----->", batch)
+        return {
+            Id: index.toString(),
+            MessageBody: JSON.stringify(batch),
+        }
+
+    });
+    console.log("sqsMessagePayloads---->>>", sqsMessagePayloads)
+
+    for (let sqsBatchPayload of chunk(sqsMessagePayloads, awsBatchSize)) {//2 чанка  1) 200 2)1
+
+        const params = {
             QueueUrl: process.env.SQS_QUEUE_URL,
-            Entries: [],
-        };
-
-        for (let j = 0; j < 10 && i < items.length; j++, i++) {
-            params.Entries.push({
-                Id: i.toString(),
-                MessageBody: JSON.stringify({
-                    userId: items[i].item.UserAccountId,
-                    field: 'OptedInThirtyDaysCancellation',
-                    value: items[i].item.Preferences.OptedInThirtyDaysCancellation,
-                }),
-            });
+            Entries: sqsBatchPayload,
         }
 
         console.log("sqs.sendMessageBatch(params).+++++", params.Entries);
@@ -101,56 +150,15 @@ const sendItemsToSqs = async (items) => {
         await sqs.sendMessageBatch(params).promise();
     }
 
-
 };
 
-//
-//
-// const sendItemsToSqs = async (items) => {
-//     let successCount = 0;
-//     const messageBatchSize = 200
-//     const awsBatchSize = 10
-//
-//     const messageBatches = chunk(items, messageBatchSize);
-//
-//     console.log("messageBatches----->", messageBatches[0])
-//
-//     const sqsMessagePayloads = messageBatches.map(batch => {
-//         console.log("batch----->", batch)
-//         return batch.map(item => {
-//                 console.log("item----->", item)
-//                 return {
-//                     Id: item.item.UserAccountId,
-//                     MessageBody: JSON.stringify({
-//                         userId: item.item.UserAccountId,
-//                         field: 'OptedInThirtyDaysCancellation',
-//                     }),
-//                 }
-//             }
-//         );
-//     });
-//     console.log("sqsMessagePayloads---->>>", sqsMessagePayloads)
-//
-//     for (let sqsBatchPayload of chunk(sqsMessagePayloads, awsBatchSize)) {
-//         for (let batch of sqsBatchPayload) {
-//             const params = {
-//                 Entries: batch,
-//                 QueueUrl: process.env.SQS_QUEUE_URL,
-//             };
-//             await sqs.sendMessageBatch(params).promise();
-//             successCount += batch.length;
-//         }
-//     }
-//     return successCount;
-// };
-//
-// const chunk = (array, size) => {
-//     let results = [];
-//     while (array.length) {
-//         results.push(array.splice(0, size));
-//     }
-//     return results;
-// };
+const chunk = (array, size) => {
+    let results = [];
+    while (array.length) {
+        results.push(array.splice(0, size));
+    }
+    return results;
+};
 
 
 // const sendItemsToSqs = async (items) => {
--- functions/process-users.js
+++ functions/process-users.js
@@ -6,14 +6,24 @@ const chunkSize = 25;
 const handleUsers = async (users, statement) => {
     let usersToUpdate = [];
 
-    for (let chunk of chunkArray(users, chunkSize)) {
+    for (let chunk of chunkArray(users, 25)) {
+        console.log("Statementusers------>", users)
+        console.log("chunkArray(users, chunkSize)------>", chunkArray(users, 25))
+        console.log(" for (let chunk of ------>", chunk)
+
         const params = {
-            Statements: chunk.map(user => ({
-                Statement: statement(user)
-            })),
+            Statements: chunk.map(user => {
+                console.log("Statementuser------>", user)
+
+                return {
+                    Statement: statement(user)
+                }
+            }),
         };
 
+
         try {
+            console.log("params.Statements--->", params.Statements)
             const response = await dynamoDB.batchExecuteStatement(params).promise();
 
             response.Responses.forEach((res, idx) => {
@@ -29,6 +39,7 @@ const handleUsers = async (users, statement) => {
     return usersToUpdate;
 };
 
+
 const chunkArray = (array, size) => {
     const chunks = [];
     for (let i = 0; i < array.length; i += size) {
@@ -37,14 +48,51 @@ const chunkArray = (array, size) => {
     return chunks;
 };
 
-module.exports.handler = async (event) => {
-    const users = event.Records.map((record) => JSON.parse(record.body));
 
-    const insertStatement = (user) => `INSERT INTO UsersBulkScriptVitaliyJadex VALUE {'UserAccountId': '${user.userId}', 'LastUpdated': '${new Date().toISOString()}'}`;
-    let usersToUpdate = await handleUsers(users, insertStatement);
+module.exports.handler = async (event) => {
+
+    console.log("event--->", event)
+    const users = event.Records.map((record) => JSON.parse(record.body));
+    console.log("users---->", users)
+
+    const insertStatement = (user) => {
+
+        console.log("userstatement: ", user);
+
+        return `INSERT INTO UsersBulkScriptVitaliyJadex VALUE {'UserAccountId': '${user.userId}', 'LastUpdated': '${new Date().toISOString()}'}`
+
+    };
+    console.log("insertStatement--->", insertStatement)
+
+
+    let usersToUpdate = []
+
+    for (const user of users) {
+        console.log("usersToUpdate.push(await handleUsers(user, insertStatement))", user)
+        usersToUpdate.push(await handleUsers(user, insertStatement))
+
+    }
 
     if (usersToUpdate.length > 0) {
         const updateStatement = (user) => `UPDATE UsersBulkScriptVitaliyJadex SET LastUpdated = '111111' WHERE UserAccountId = '${user.userId}'`;
         await handleUsers(usersToUpdate, updateStatement);
+
+        for (const user of usersToUpdate) {
+            console.log("usersToUpdate.push(await handleUsers(user, updateStatement))", user)
+            await handleUsers(user, updateStatement)
+
+        }
     }
 };
+
+
+
+
+
+
+
+
+
+
+
+
